bf06694cf2f8493c9931a2954c0219ddde337ba5
TURX <turx2003@gmail.com>
2020-02-08 03:06:24 +0800

    optimize model and prediction

--- model.py
+++ model.py
@@ -1,17 +1,17 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 from pathlib import Path
-from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, Dense, Dropout, Flatten
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, Dense, Dropout, Flatten, MaxPooling2D, ZeroPadding2D, Convolution2D, Input
+from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import os
-import pathlib
-import random
 import tensorflow as tf
 
 tf.compat.v1.enable_eager_execution()
 
-config = tf.ConfigProto()
+config = tf.ConfigProto(allow_soft_placement=True)
 config.gpu_options.allow_growth = True
+config.gpu_options.allocator_type = 'BFC'
+config.gpu_options.per_process_gpu_memory_fraction = 3.0
 session = tf.Session(config=config)
 
 train_data_root = Path("./data/train")
@@ -43,36 +43,33 @@ print("Label to index:", label_to_index)
 print("All train image paths:", all_train_image_paths)
 print("All validation image paths:", all_validation_image_paths)
 
-BATCH_SIZE = 2
+BATCH_SIZE = 4
+if tf.keras.backend.image_data_format() == 'channels_first':
+    input_shape = (3, 512, 384)
+else:
+    input_shape = (512, 384, 3)
+
+output_shape = (512, 384)
 
 print("Batch size:", BATCH_SIZE)
 
 model = Sequential()
-
-model.add(Conv2D(64, 3, strides=3, input_shape=(256, 256, 3)))
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
 model.add(Activation(tf.nn.leaky_relu))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(32, 3, strides=2))
+model.add(Conv2D(32, (3, 3)))
 model.add(Activation(tf.nn.leaky_relu))
-model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(32, 3, strides=2))
+model.add(Conv2D(64, (3, 3)))
 model.add(Activation(tf.nn.leaky_relu))
-model.add(Dropout(0.1))
-model.add(BatchNormalization())
-
-model.add(Conv2D(16, 3, strides=1))
-model.add(Activation(tf.nn.leaky_relu))
-model.add(Dropout(0.1))
-model.add(BatchNormalization())
-
-model.add(Conv2D(1, 3, strides=1))
-model.add(Activation(tf.nn.leaky_relu))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())
 model.add(Dense(64))
 model.add(Activation(tf.nn.leaky_relu))
-
+model.add(Dropout(0.2))
 model.add(Dense(len(label_names)))
 model.add(Activation('softmax'))
 
@@ -85,13 +82,18 @@ train_datagen = ImageDataGenerator(
 validation_datagen = ImageDataGenerator(rescale=1. / 255)
 
 train_generator = train_datagen.flow_from_directory(train_data_root, target_size=(
-    256, 256), batch_size=BATCH_SIZE, class_mode='categorical')
+    output_shape), batch_size=BATCH_SIZE, class_mode='categorical')
 
 validation_generator = validation_datagen.flow_from_directory(
-    validation_data_root, target_size=(256, 256), batch_size=BATCH_SIZE, class_mode='categorical')
+    validation_data_root, target_size=(output_shape), batch_size=BATCH_SIZE, class_mode='categorical')
 
-model.fit_generator(train_generator, steps_per_epoch=train_image_count // BATCH_SIZE, epochs=600,
-                    validation_data=validation_generator, validation_steps=validation_image_count // BATCH_SIZE)
+filepath = "model-improvement-{epoch:02d}-{val_acc:.2f}.h5"
+checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=0,
+                             save_best_only=False, save_weights_only=False, mode='auto', period=1)
+callbacks_list = [checkpoint]
+
+model.fit_generator(train_generator, steps_per_epoch=train_image_count // BATCH_SIZE, epochs=30,
+                    validation_data=validation_generator, validation_steps=validation_image_count // BATCH_SIZE, callbacks=callbacks_list)
 
 model.summary()
 
